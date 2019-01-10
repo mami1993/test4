9fdb31c1cb1b0df7d1695c3bdbda40bc947b9e0b
mami1993 <44319251+mami1993@users.noreply.github.com>
2019-01-10 18:20:12 +0000

    Update FileUploadService.java

--- src/main/java/com/javatpoint/rest/FileUploadService.java
+++ src/main/java/com/javatpoint/rest/FileUploadService.java
@@ -21,7 +21,7 @@ public class FileUploadService {
 	public Response uploadFile(@FormDataParam("file") InputStream uploadedInputStream,
 			@FormDataParam("file") FormDataContentDisposition fileDetail) {
 
-		String fileLocation = "/app/" + fileDetail.getFileName();
+		String fileLocation = "/usr/share/doc/mount/" + fileDetail.getFileName();
 		// saving file
 		String output = " ";
 		try {
